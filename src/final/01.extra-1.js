9b66040a830ceb4b4b8c3aa720a947461312a967
jin young <jinnyxchoi@gmail.com>
2021-05-28 15:07:11 -0400

    countReducer

--- src/final/01.extra-1.js
+++ src/final/01.extra-1.js
@@ -8,6 +8,7 @@ const countReducer = (count, change) => count + change
 
 function Counter({initialCount = 0, step = 1}) {
   const [count, changeCount] = React.useReducer(countReducer, initialCount)
+
   const increment = () => changeCount(step)
   return <button onClick={increment}>{count}</button>
 }
