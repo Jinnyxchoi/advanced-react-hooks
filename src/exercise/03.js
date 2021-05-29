9d4011c325122ede236195f44206888aa6a83f61
jin young <jinnyxchoi@gmail.com>
2021-05-29 13:44:46 -0400

    adding useCount function

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -11,17 +11,21 @@ function CountProvider(props) {
   return <CountContext.Provider value={value} {...props} />
 }
 
-//   🐨 return your context provider with the value assigned to that array and forward all the other props
-//   💰 more specifically, we need the children prop forwarded to the context provider
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error(`useCount must be used within a CountProvider`)
+  }
+  return context
+}
 
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  // 🐨 get the setCount from useContext with the CountContext
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
